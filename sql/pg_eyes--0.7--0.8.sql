563a70b0d9fc3d065d4dd476e698c5e8f77fa360
Alexander Samoylov <amsamoylov@yandex.ru>
2017-05-01 16:23:11 +0300

    Обновлены версии расширения pg_eyes до 0.8(9.4, 9.5) и 1.3(9.6)
    Версия по умолчанию(default_version) изменена на 1.3
    
    1 Изменена функция eyes.get_activity
    1.1 Изменены метрики: pg_stat_activity.active_time, pg_stat_activity.dba_task,
        pg_stat_activity.dba_task_time.  При расчете случая с
        application_name = "DBA Task" убрано ограничение client_addr IS NULL
    
    изменено:      pg_eyes.control
    новый файл:    sql/pg_eyes--0.7--0.8.sql
    новый файл:    sql/pg_eyes--0.8.sql
    новый файл:    sql/pg_eyes--1.2--1.3.sql
    новый файл:    sql/pg_eyes--1.3.sql

---
+++ sql/pg_eyes--0.7--0.8.sql
@@ -0,0 +1,371 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pg_eyes" to load this file. \quit
+
+
+-- Function: eyes.get_activity();
+
+/*
+Изменены метрики: pg_stat_activity.active_time, pg_stat_activity.dba_task,
+    pg_stat_activity.dba_task_time.  При расчете случая с
+    application_name = "DBA Task" убрано ограничение client_addr IS NULL
+*/
+
+CREATE OR REPLACE FUNCTION eyes.get_activity()
+    RETURNS TABLE(stat_name character varying,
+        stat_value bigint
+    ) AS
+$body$
+DECLARE
+
+/*
+Описание:
+    Получение метрик для систем мониторинга
+*/
+
+    -- pg_stat_activity
+    v_total bigint;
+    v_active bigint;
+    v_active_1s bigint;
+    v_active_time bigint;
+    v_idle bigint;
+    v_idle_in_tr bigint;
+    v_idle_in_tr_1s bigint;
+    v_idle_in_tr_time bigint;
+    v_xact_time bigint;
+    v_wait_lock bigint;
+    v_wait_lock_1s bigint;
+    v_wait_lock_time bigint;
+    v_autovacuum bigint;
+    v_autovacuum_time bigint;
+    v_dba_task bigint;
+    v_dba_task_time bigint;    
+
+    -- pg_stat_database
+    v_backends_pct bigint;
+    v_xact_total bigint;
+    v_xact_commit bigint;
+    v_xact_rollback bigint;
+    v_blks_read bigint;
+    v_blks_hit bigint;
+    v_tup_returned bigint;
+    v_tup_fetched bigint;
+    v_tup_inserted bigint;
+    v_tup_updated bigint;
+    v_tup_deleted bigint;
+    v_conflicts bigint;
+    v_temp_files bigint;
+    v_temp_bytes bigint;
+    v_deadlocks bigint;
+
+    -- pg_stat_database
+    v_checkpoints_timed bigint;
+    v_checkpoints_req bigint;
+    v_checkpoint_write_time bigint;
+    v_checkpoint_sync_time bigint;
+    v_buffers_checkpoint bigint;
+    v_buffers_clean bigint;
+    v_maxwritten_clean bigint;
+    v_buffers_backend bigint;
+    v_buffers_backend_fsync bigint;
+    v_buffers_alloc bigint;
+
+    -- replication state
+    v_last_xact_replay_timestamp timestamp with time zone;
+    v_clock_timestamp timestamp with time zone;
+    v_is_in_recovery bigint;
+    v_ms_lag bigint;
+    v_last_update_status bigint;
+    
+    -- wal
+    v_wal_written_b bigint;
+
+    r record;
+
+BEGIN
+
+    -- Get pg_stat_activity
+
+    SELECT count(1) AS total,
+        count(*) FILTER (WHERE state = 'active') AS active,
+        count(*) FILTER (WHERE state = 'active'
+            AND now() - query_start > interval '1s') AS active_1s,
+        coalesce(max(round(extract(epoch FROM age(statement_timestamp(),
+            query_start))*1000)) FILTER (WHERE state = 'active'
+            AND query NOT LIKE 'autovacuum:%'
+            AND application_name != 'DBA Task'
+            ), 0) AS active_time,
+        count(*) FILTER (WHERE state = 'idle') AS idle,
+        count(*) FILTER (WHERE state LIKE 'idle in%') AS idle_in_tr,
+        count(*) FILTER (WHERE state LIKE 'idle in%'
+            AND statement_timestamp() - state_change > interval '1s')
+            AS idle_in_tr_1s,
+        coalesce(max(round(extract(epoch FROM age(statement_timestamp(),
+            state_change))*1000)) FILTER (WHERE state LIKE 'idle in%'),
+            0) AS idle_in_tr_time,      
+        coalesce(max(round(extract(epoch FROM age(statement_timestamp(),
+            xact_start))*1000)) FILTER (WHERE state != 'idle'
+            AND query NOT LIKE 'autovacuum:%'), 0) AS xact_time,
+        count(*) FILTER (WHERE waiting) AS wait_lock,
+        count(*) FILTER (WHERE waiting
+            AND statement_timestamp() - query_start > interval '1s')
+            AS wait_lock_1s,   
+        coalesce(max(round(extract(epoch FROM age(statement_timestamp(),
+            query_start))*1000)) FILTER (WHERE waiting),
+            0) AS wait_lock_time,    
+        count(*) FILTER (WHERE state = 'active' AND query LIKE 'autovacuum:%')
+            AS autovacuum,
+        coalesce(max(round(extract(epoch FROM age(statement_timestamp(),
+            query_start))*1000)) FILTER (WHERE state = 'active'
+            AND query LIKE 'autovacuum:%'), 0) AS autovacuum_time,
+        count(*) FILTER (WHERE state = 'active'
+            AND application_name = 'DBA Task')
+            AS dba_task,
+        coalesce(max(round(extract(epoch FROM age(statement_timestamp(),
+            query_start))*1000)) FILTER (WHERE state = 'active'
+            AND application_name = 'DBA Task'), 0)
+            AS dba_task_time
+        INTO v_total,
+            v_active, v_active_1s, v_active_time,
+            v_idle, v_idle_in_tr, v_idle_in_tr_1s, v_idle_in_tr_time,
+            v_xact_time, v_wait_lock, v_wait_lock_1s, v_wait_lock_time,
+            v_autovacuum, v_autovacuum_time, v_dba_task, v_dba_task_time
+    FROM pg_stat_activity;
+
+    stat_name := 'pg_stat_activity.total';
+    stat_value := v_total;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.active';
+    stat_value := v_active;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.active_1s';
+    stat_value := v_active_1s;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.active_time';
+    stat_value := v_active_time;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.idle';
+    stat_value := v_idle;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.idle_in_tr';
+    stat_value := v_idle_in_tr;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.idle_in_tr_1s';
+    stat_value := v_idle_in_tr_1s;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.idle_in_tr_time';
+    stat_value := v_idle_in_tr_time;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.xact_time';
+    stat_value := v_xact_time;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.wait_lock';
+    stat_value := v_wait_lock;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.wait_lock_1s';
+    stat_value := v_wait_lock_1s;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.wait_lock_time';
+    stat_value := v_wait_lock_time;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.autovacuum';
+    stat_value := v_autovacuum;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.autovacuum_time';
+    stat_value := v_autovacuum_time;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.dba_task';
+    stat_value := v_dba_task;
+    RETURN NEXT;
+    stat_name := 'pg_stat_activity.dba_task_time';
+    stat_value := v_dba_task_time;
+    RETURN NEXT;
+
+    -- Get pg_stat_database
+
+    SELECT round(
+            100*(sum(numbackends)/current_setting('max_connections')::numeric),
+            0) AS backends_pct,
+        sum(xact_commit + xact_rollback) AS xact_total,
+        sum(xact_commit) AS xact_commit,
+        sum(xact_rollback) AS xact_rollback,
+        sum(blks_read) AS blks_read,
+        sum(blks_hit) AS blks_hit,
+        sum(tup_returned) AS tup_returned,
+        sum(tup_fetched) AS tup_fetched,
+        sum(tup_inserted) AS tup_inserted,
+        sum(tup_updated) AS tup_updated,
+        sum(tup_deleted) AS tup_deleted,
+        sum(conflicts) AS conflicts,
+        sum(temp_files) AS temp_files,
+        sum(temp_bytes) AS temp_bytes,
+        sum(deadlocks) AS deadlocks
+        INTO v_backends_pct, v_xact_total, v_xact_commit, v_xact_rollback,
+            v_blks_read, v_blks_hit, v_tup_returned, v_tup_fetched,
+            v_tup_inserted, v_tup_updated, v_tup_deleted, v_conflicts,
+            v_temp_files, v_temp_bytes, v_deadlocks
+    FROM pg_stat_database;
+
+    stat_name := 'pg_stat_database.backends_pct';
+    stat_value := v_backends_pct;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.xact_total';
+    stat_value := v_xact_total;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.xact_commit';
+    stat_value := v_xact_commit;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.xact_rollback';
+    stat_value := v_xact_rollback;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.blks_read';
+    stat_value := v_blks_read;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.blks_hit';
+    stat_value := v_blks_hit;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.tup_returned';
+    stat_value := v_tup_returned;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.tup_fetched';
+    stat_value := v_tup_fetched;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.tup_inserted';
+    stat_value := v_tup_inserted;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.tup_updated';
+    stat_value := v_tup_updated;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.tup_deleted';
+    stat_value := v_tup_deleted;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.conflicts';
+    stat_value := v_conflicts;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.temp_files';
+    stat_value := v_temp_files;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.temp_bytes';
+    stat_value := v_temp_bytes;
+    RETURN NEXT;
+    stat_name := 'pg_stat_database.deadlocks';
+    stat_value := v_deadlocks;
+    RETURN NEXT;
+
+    -- Get pg_stat_bgwriter
+
+    SELECT checkpoints_timed AS checkpoints_timed,
+        checkpoints_req AS checkpoints_req,
+        checkpoint_write_time AS checkpoint_write_time,
+        checkpoint_sync_time AS checkpoint_sync_time,
+        buffers_checkpoint AS buffers_checkpoint,
+        buffers_clean AS buffers_clean,
+        maxwritten_clean AS maxwritten_clean,
+        buffers_backend AS buffers_backend,
+        buffers_backend_fsync AS buffers_backend_fsync,
+        buffers_alloc AS buffers_alloc
+        INTO v_checkpoints_timed, v_checkpoints_req, v_checkpoint_write_time,
+            v_checkpoint_sync_time, v_buffers_checkpoint, v_buffers_clean,
+            v_maxwritten_clean, v_buffers_backend, v_buffers_backend_fsync,
+            v_buffers_alloc
+    FROM pg_stat_bgwriter;
+
+    stat_name := 'pg_stat_bgwriter.checkpoints_timed';
+    stat_value := v_checkpoints_timed;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.checkpoints_req';
+    stat_value := v_checkpoints_req;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.checkpoint_write_time';
+    stat_value := v_checkpoint_write_time;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.checkpoint_sync_time';
+    stat_value := v_checkpoint_sync_time;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.buffers_checkpoint';
+    stat_value := v_buffers_checkpoint;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.buffers_clean';
+    stat_value := v_buffers_clean;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.maxwritten_clean';
+    stat_value := v_maxwritten_clean;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.buffers_backend';
+    stat_value := v_buffers_backend;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.buffers_backend_fsync';
+    stat_value := v_buffers_backend_fsync;
+    RETURN NEXT;
+    stat_name := 'pg_stat_bgwriter.buffers_alloc';
+    stat_value := v_buffers_alloc;
+    RETURN NEXT;
+
+    -- Get replication state
+
+    IF NOT pg_is_in_recovery() THEN
+        -- Если выполняется на primary
+
+        v_is_in_recovery := 0;
+        v_ms_lag := 0;
+
+        -- Искуственный апдейт для проверки репликации
+        v_last_update_status := eyes.update_time();
+
+        -- Объем данных, записанных в WAL
+        v_wal_written_b := pg_catalog.pg_xlog_location_diff(
+            pg_catalog.pg_current_xlog_location(), '0/00000000');
+        stat_name := 'wal_written_b';
+        stat_value := v_wal_written_b;
+        return next;
+
+        -- Проверка отставания репликации
+        FOR r IN SELECT application_name AS application_name,
+            trunc(pg_xlog_location_diff(sent_location, replay_location)
+                ) AS b_lag
+            FROM pg_stat_replication
+            WHERE state = 'streaming'
+        LOOP
+            stat_name := 'replication.streaming_'||r.application_name||'_b_lag';
+            stat_value := r.b_lag;
+            return next;
+        END LOOP;
+
+    ELSE
+        -- Если выполняется на standby
+
+        v_is_in_recovery := 1;
+        v_last_xact_replay_timestamp := pg_last_xact_replay_timestamp();
+        v_clock_timestamp := clock_timestamp();
+        v_ms_lag := trunc(extract(epoch FROM age(v_clock_timestamp,
+            coalesce(v_last_xact_replay_timestamp, v_clock_timestamp)))*1000);
+
+    END IF;
+
+    stat_name := 'replication.is_in_recovery';
+    stat_value := v_is_in_recovery;
+    RETURN NEXT;
+    stat_name := 'replication.ms_lag';
+    stat_value := v_ms_lag;
+    RETURN NEXT;
+
+    -- Время ответа функции
+    stat_name := 'response_time';
+    stat_value := trunc(extract(epoch FROM age(clock_timestamp(),
+            current_timestamp))*1000);
+    RETURN NEXT;   
+
+    RETURN;
+
+END;
+$body$
+    LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+    COST 100;
+
+ALTER FUNCTION eyes.get_activity()
+    OWNER TO postgres;
+
+COMMENT ON FUNCTION eyes.get_activity()
+    IS 'Получение метрик для систем мониторинга';
+
+
+-- END
